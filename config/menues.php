0fdd1079e578897b5313959fddad89aa56d2e5e2
alessandrodaniello105 <kanymsevement@gmail.com>
2023-11-17 16:08:04 +0100

    [add]: menues.php in \config\

---
+++ config/menues.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+    "navHeader" => [
+        [
+            "name" => "uomo",
+            "text" => "Uomo"
+        ],
+        [
+            "name" => "donna",
+            "text" => "Donna"
+        ],
+        [
+            "name" => "bambini",
+            "text" => "Bambini"
+        ]
+        ],
+    "menuHeader" => [
+        [
+            "class" => "fa-regular fa-user"
+        ],
+        [
+            "class" => "fa-regular fa-heart"
+        ],
+        [
+            "class" => "fa-solid fa-bag-shopping"
+        ]
+    ]
+]
+
+?>
